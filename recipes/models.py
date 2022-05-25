4a2e8904752a986b994b7f73959a57a5fe0e564e
MariaShchegl <mar.code.shchegl@gmail.com>
2022-05-25 22:54:44 +0300

    adminPanel+fixModels+configureRelations

--- recipes/models.py
+++ recipes/models.py
@@ -2,22 +2,42 @@ from django.db import models
 
 # Create your models here.
 
+class TextFieldImpl(models.TextField):
+    "Implements comma-separated storage of lists"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def db_type(self, connection):
+        return 'TEXT'
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+
+        return name, path, args, kwargs
+
 class User(models.Model):
     login = models.CharField(max_length=30)
     password = models.CharField(max_length=128)
     email = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.login
 
 class Image(models.Model):
-    path = models.Field()
+    pathImage = models.ImageField()
+    def __str__(self):
+        return self.pathImage
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    data = models.Field()
+    description = TextFieldImpl(default="")
     image = models.OneToOneField(Image,
         on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
     class Meta:
         indexes = [
             models.Index(fields=['created_at'], name='recipe_idx'),
@@ -27,6 +47,8 @@ class Category(models.Model):
     title = models.CharField(max_length=30)
     alias = models.CharField(max_length=30)
     recipe = models.ManyToManyField(Recipe)
+    def __str__(self):
+        return self.title
     class Meta:
         indexes = [
             models.Index(fields=['id'], name='category_idx'),
@@ -36,7 +58,9 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    data = models.CharField(max_length=750)
+    data = TextFieldImpl(default="")
+    def __str__(self):
+        return self.data
     class Meta:
         indexes = [
             models.Index(fields=['created_at'], name='comment_idx'),
